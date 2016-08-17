1fa944c87837de024a56125947d8bb7dcd278db0
JGMorgan <josegm11194@gmail.com>
2016-08-17 01:43:11 -0400

    changed matrix.js into ml_math.js which is a module with a bunch of math functions that are useful for machine learning

--- ml_math.js
+++ ml_math.js
@@ -10,6 +10,33 @@ var transpose = (x) => {
     return out;
 }
 
+var determinant = (x) => {
+    if(x.length === 2){
+        return x[0][0] * x[1][1] - x[1][0] * x[0][1];
+    }else if(x.length === 3){
+        //|A| = a(ei - fh) - b(di - fg) + c(dh - eg)
+        return x[0][0] * (x[1][1] * x[2][2] - x[1][2] * x[2][1])
+             - x[0][1] * (x[1][0] * x[2][2] - x[1][2] * x[2][0])
+             + x[0][2] * (x[1][0] * x[2][1] - x[1][1] * x[2][0]);
+    }else{
+        var sum = 0;
+        for (var i = 0; i < x[0].length; i++){
+            var xCpy = clone(x);
+            var temp = [];
+            for (var j = 1; j < x.length; j++){
+                temp.push(xCpy[j].slice(0));
+                temp[j-1].splice(i, 1);
+            }
+            if((i % 2) === 0){
+                sum = sum + (x[0][i] * determinant(temp));
+            }else{
+                sum = sum - (x[0][i] * determinant(temp));
+            }
+        }
+        return sum;
+    }
+}
+
 var inverse = (x) => {
     var clone = (x) => {
         var out = [];
@@ -17,32 +44,6 @@ var inverse = (x) => {
             out.push(x[i].slice(0));
         }
         return out;
-    }
-    var determinant = (x) => {
-        if(x.length === 2){
-            return x[0][0] * x[1][1] - x[1][0] * x[0][1];
-        }else if(x.length === 3){
-            //|A| = a(ei - fh) - b(di - fg) + c(dh - eg)
-            return x[0][0] * (x[1][1] * x[2][2] - x[1][2] * x[2][1])
-                 - x[0][1] * (x[1][0] * x[2][2] - x[1][2] * x[2][0])
-                 + x[0][2] * (x[1][0] * x[2][1] - x[1][1] * x[2][0]);
-        }else{
-            var sum = 0;
-            for (var i = 0; i < x[0].length; i++){
-                var xCpy = clone(x);
-                var temp = [];
-                for (var j = 1; j < x.length; j++){
-                    temp.push(xCpy[j].slice(0));
-                    temp[j-1].splice(i, 1);
-                }
-                if((i % 2) === 0){
-                    sum = sum + (x[0][i] * determinant(temp));
-                }else{
-                    sum = sum - (x[0][i] * determinant(temp));
-                }
-            }
-            return sum;
-        }
     }
     var minors = (x) => {
         var out = [];
@@ -109,4 +110,51 @@ var dot = (x, y) => {
     return out;
 }
 
-module.exports = {dot, inverse, transpose};
+var reLU = (x) => {
+    if (x > 0) {
+        return x;
+    }else{
+        return 0;
+    }
+}
+
+var sigmoid = (x) => {
+    return (1 / (1 + Math.pow(Math.E, -x)));
+}
+
+var sigmoidPrime = (x) => {
+    var eNegX = Math.Pow(Math.E, -x);
+    return (eNegX / Math.pow(1 + eNegX, 2));
+}
+
+var tanh = (x) => {
+    var eX = Math.pow(Math.E, x);
+    var eNegX = Math.pow(Math.E, -x);
+    return (eX - eNegX) / (eX + eNegX);
+}
+
+var tanhPrime = (x) => {
+    var tanhx = tanh(x);
+    return 1 - (tanhx * tanhx)
+}
+
+var linear = (x) => {
+    return x;
+}
+
+var linearPrime = (x) => {
+    return 1;
+}
+
+var distance = (x, y) => {
+    var sum = 0;
+    for (var i = 0; i < x.length; i++){
+        sum += Math.pow(x[i] - y[i], 2);
+    }
+    return Math.sqrt(sum);
+}
+
+module.exports = {dot, determinant, inverse, transpose, reLU, sigmoid,
+                  sigmoidPrime, tanh, tanhPrime, linear, linearPrime,
+                  distance
+                 };
