b046f426af47faf82eb01603d0a9fbb8ffe93e4f
JGMorgan <josegm11194@gmail.com>
2016-08-19 22:38:07 -0400

    removed extra files

--- logistic_regression.js
+++
@@ -1,8 +0,0 @@
-var mlmath = require('./ml_math.js');
-var LinearRegression = require('./linear_regression.js');
-class LogisticRegression extends LinearRegression.LinearRegression{
-    predict(X){
-        return mlmath.sigmoid(super.predict(X));
-    }
-}
-module.exports = {LogisticRegression};
